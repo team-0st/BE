9ac8500ac6826e6f4cc99ad7b3f82483e06d2d33
JoonKyoLee <joonkyolee@gmail.com>
2026-07-14 23:04:49 +0900

    fix: EC2 교체 없이 탄력적 IP 연결 구성으로 조정

--- terraform/main.tf
+++ terraform/main.tf
@@ -168,7 +168,7 @@ resource "aws_instance" "app" {
   subnet_id                   = aws_subnet.public[local.primary_public_subnet_key].id
   vpc_security_group_ids      = [aws_security_group.app.id]
   key_name                    = var.key_name
-  associate_public_ip_address = false
+  associate_public_ip_address = true
   user_data = templatefile("${path.module}/user_data.sh.tftpl", {
     app_port = var.app_port
   })
