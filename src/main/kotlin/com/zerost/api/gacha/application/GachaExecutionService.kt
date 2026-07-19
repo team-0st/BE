250f3489f119599cc2d96219318cfd827af2700b
JoonKyoLee <joonkyolee@gmail.com>
2026-07-20 01:24:14 +0900

    feat: 가챠 보상 지급과 재화 이력 적재 추가

--- src/main/kotlin/com/zerost/api/gacha/application/GachaExecutionService.kt
+++ src/main/kotlin/com/zerost/api/gacha/application/GachaExecutionService.kt
@@ -2,11 +2,20 @@ package com.zerost.api.gacha.application
 
 import com.zerost.api.common.exception.BusinessException
 import com.zerost.api.common.exception.ErrorCode
+import com.zerost.api.ecojam.domain.EcoJamHistory
+import com.zerost.api.ecojam.domain.EcoJamHistoryRepository
+import com.zerost.api.ecojam.domain.EcoJamHistorySourceType
 import com.zerost.api.gacha.domain.Gacha
 import com.zerost.api.gacha.domain.GachaRepository
 import com.zerost.api.gacha.domain.GachaRewardPolicy
 import com.zerost.api.gacha.domain.GachaRewardPolicyRepository
 import com.zerost.api.gacha.presentation.dto.ExecuteGachaResponse
+import com.zerost.api.ingredient.domain.UserIngredient
+import com.zerost.api.ingredient.domain.UserIngredientRepository
+import com.zerost.api.point.domain.PointHistory
+import com.zerost.api.point.domain.PointHistoryRepository
+import com.zerost.api.point.domain.PointHistorySourceType
+import com.zerost.api.user.domain.User
 import com.zerost.api.user.domain.UserRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -17,6 +26,9 @@ class GachaExecutionService(
     private val userRepository: UserRepository,
     private val gachaRewardPolicyRepository: GachaRewardPolicyRepository,
     private val gachaRepository: GachaRepository,
+    private val userIngredientRepository: UserIngredientRepository,
+    private val ecoJamHistoryRepository: EcoJamHistoryRepository,
+    private val pointHistoryRepository: PointHistoryRepository,
     private val gachaRandomProvider: GachaRandomProvider,
 ) {
 
@@ -51,6 +63,9 @@ class GachaExecutionService(
                 resultIngredientQuantity = selectedPolicy.ingredientQuantity,
             ),
         )
+
+        applyReward(user, selectedPolicy)
+        saveHistories(user, gacha)
 
         return ExecuteGachaResponse(
             gachaId = requireNotNull(gacha.id),
@@ -89,6 +104,72 @@ class GachaExecutionService(
         }
 
         throw BusinessException(ErrorCode.INVALID_GACHA_REWARD_POLICY)
+    }
+
+    private fun applyReward(
+        user: User,
+        selectedPolicy: GachaRewardPolicy
+    ) {
+        if (selectedPolicy.pointAmount > 0) {
+            user.increasePoint(selectedPolicy.pointAmount)
+        }
+
+        if (selectedPolicy.ecoJamAmount > 0) {
+            user.increaseEcoJam(selectedPolicy.ecoJamAmount)
+        }
+
+        val ingredient = selectedPolicy.ingredient
+        if (ingredient != null && selectedPolicy.ingredientQuantity > 0) {
+            val userIngredient = userIngredientRepository.findByUserAndIngredient(user, ingredient)
+                .orElseGet {
+                    UserIngredient(
+                        user = user,
+                        ingredient = ingredient,
+                        quantity = 0,
+                    )
+                }
+
+            userIngredient.increaseQuantity(selectedPolicy.ingredientQuantity)
+            userIngredientRepository.save(userIngredient)
+        }
+    }
+
+    private fun saveHistories(
+        user: User,
+        gacha: Gacha
+    ) {
+        val gachaId = requireNotNull(gacha.id)
+
+        ecoJamHistoryRepository.save(
+            EcoJamHistory.spend(
+                user = user,
+                amount = gacha.costEcoJam,
+                sourceType = EcoJamHistorySourceType.GACHA,
+                sourceId = gachaId,
+            )
+        )
+
+        if (gacha.resultEcoJam > 0) {
+            ecoJamHistoryRepository.save(
+                EcoJamHistory.earn(
+                    user = user,
+                    amount = gacha.resultEcoJam,
+                    sourceType = EcoJamHistorySourceType.GACHA,
+                    sourceId = gachaId,
+                )
+            )
+        }
+
+        if (gacha.resultPoint > 0) {
+            pointHistoryRepository.save(
+                PointHistory.earn(
+                    user = user,
+                    amount = gacha.resultPoint,
+                    sourceType = PointHistorySourceType.GACHA,
+                    sourceId = gachaId,
+                )
+            )
+        }
     }
 
     companion object {
